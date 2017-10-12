0f43866cbe223b545392ee7ffcefa4d5e9352e07
PatJohnMartin <32583552+PatJohnMartin@users.noreply.github.com>
2017-10-12 16:30:10 -0700

    Update Figure1x.m

--- Figure1x.m
+++ Figure1x.m
@@ -1,13 +1,12 @@
 close all; % closes all open figure windows
 
 
-Y1 = [0, 4.37E+05, 1.07E+06, 2.50E+06, 2.51E+06, 2.06E+06, 3.18E+06]; % Throughput X, CSMA:CA 1, Scenario A
-Y2 = [0, 6.56E+05, 1.07E+06, 2.70E+06, 2.91E+06, 3.35E+06, 2.69E+06]; % Throughput X, CSMA:CA 2, Scenario A
+Y1 = [0,0.3289571702,0.2335796125,0.2563993665,0.2585656756,0.2938522785,0.3809345028]; % Throughput A/B or Fairness Index, CSMA:CA 1, Scenario A
+Y2 = [0,0.9761273808,0.9734293421,0.925899101,0.923288039,0.8197300308,0.547829298]; %Throughput A/B or Fairness Index, CSMA:CA 2, Scenario A
+Y3 = [0,0.7985626257,0.3744301573,0.4139054679,0.4474408753,0.4274162353,0.3628596592]; % Throughput A/B or Fairness Index, CSMA:CA 1, Scenario B
+Y4 = [0,0.44453857,0.3350368194,0.2518266725,0.4087353879,0.5006991499,0.5159186066]; % Throughput A/B or Fairness Index, CSMA:CA 2, Scenario B
 
-Y3 = [0, 5.79E+05, 1.01E+06, 2.41E+06, 2.96E+06, 2.82E+06, 2.81E+06]; % Throughput X, CSMA:CA 1, Scenario B
-Y4 = [0, 2.74E+05, 6.11E+05, 1.09E+06, 1.30E+06, 2.37E+06, 2.47E+06]; % Throughput X, CSMA:CA 2, Scenario B
-
-X = [0, 50, 100, 200, 300, 400, 500]; % Lambda
+X = [0,50, 100, 200, 300, 400, 500]; % Lambda Values
 
 set(0,'defaulttextinterpreter','latex'); % allows you to use latex math
 set(0,'defaultlinelinewidth',2); % line width is set to 2
@@ -16,8 +15,9 @@ set(0,'DefaultTextFontSize', 16); % Font size is set to 16
 set(0,'DefaultAxesFontSize',16); % font size for the axes is set to 16
 
 figure(1)
-plot(X, Y1, X, Y2, X, Y3, X, Y4); % plotting four curves Y1, Y2, Y3, Y4 for the same X value
+%title = "Figure 1A"
+plot(X, Y1, X, Y2, X, Y3, X, Y4); % plotting three curves Y1, Y2 for the same X
 grid on; % grid lines on the plot
 legend('CSMA Scenario A', 'CSMA Scenario A with Virtual Sensing', 'CSMA Scenario B', 'CSMA Scenario B with Virtual Sensing');
-ylabel('$Throughput$ (Kbps)');
+ylabel('$Fairness-Index$'); %Throughput (Kbps) or Fairness Index (N)
 xlabel('$\lambda$ (frames/sec)');
